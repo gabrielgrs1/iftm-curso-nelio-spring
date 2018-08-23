f0ae4873d8a0b4a4f443e6393a5e3d563e8bc970
Gabriel Guilherme Rodrigues Silva <gabriel_guilherme2006@hotmail.com>
2018-08-23 19:58:46 -0300

    Secao 2 aula 12

--- aulaspring/src/main/java/com/gabrielgrs/aulaspring/resources/CategoriaResource.java
+++ aulaspring/src/main/java/com/gabrielgrs/aulaspring/resources/CategoriaResource.java
@@ -1,16 +1,28 @@
 package com.gabrielgrs.aulaspring.resources;
 
 
+import com.gabrielgrs.aulaspring.domain.Categoria;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String listar() {
-        return "REST está funcionando!";
+    public List<Categoria> listar() {
+
+        Categoria cat1 = new Categoria(1, "Informática");
+        Categoria cat2 = new Categoria(2, "Escritório");
+
+        List<Categoria> categoriaList = new ArrayList<>();
+        categoriaList.add(cat1);
+        categoriaList.add(cat2);
+
+        return categoriaList;
     }
 }
