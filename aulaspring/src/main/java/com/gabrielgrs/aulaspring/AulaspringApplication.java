5afb882634604415f7e90a0acf89ddb52b1cc95b
Gabriel Guilherme Rodrigues Silva <gabriel_guilherme2006@hotmail.com>
2018-08-23 20:51:19 -0300

    Secao 2 aula 16

--- aulaspring/src/main/java/com/gabrielgrs/aulaspring/AulaspringApplication.java
+++ aulaspring/src/main/java/com/gabrielgrs/aulaspring/AulaspringApplication.java
@@ -1,32 +1,49 @@
 package com.gabrielgrs.aulaspring;
 
 import com.gabrielgrs.aulaspring.domain.Categoria;
+import com.gabrielgrs.aulaspring.domain.Produto;
 import com.gabrielgrs.aulaspring.repositories.CategoriaRepository;
+import com.gabrielgrs.aulaspring.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 @SpringBootApplication
 public class AulaspringApplication implements CommandLineRunner {
 
-	@Autowired
-	private CategoriaRepository categoriaRepository;
-
-	public static void main(String[] args) {
-		SpringApplication.run(AulaspringApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-
-		Categoria cat1 = new Categoria(null, "Informática");
-		Categoria cat2 = new Categoria(null, "Escritório");
-
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+    @Autowired
+    private ProdutoRepository produtoRepository;
 
 
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AulaspringApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Categoria cat1 = new Categoria(null, "Informática");
+        Categoria cat2 = new Categoria(null, "Escritório");
+
+        Produto p1 = new Produto(null, "Computador", 2000.0);
+        Produto p2 = new Produto(null, "Impressora", 800.0);
+        Produto p3 = new Produto(null, "Mouse", 80.0);
+
+        cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+        cat2.getProdutos().addAll(Collections.singletonList(p2));
+
+        p1.getCategorias().addAll(Collections.singletonList(cat1));
+        p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+        p3.getCategorias().addAll(Collections.singletonList(cat1));
+
+        categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+        produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+
+    }
 }
