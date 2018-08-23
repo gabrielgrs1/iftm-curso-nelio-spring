79623e5b39a107d6c21f6fb946fe875f03a99b8f
Gabriel Guilherme Rodrigues Silva <gabriel_guilherme2006@hotmail.com>
2018-08-23 20:37:21 -0300

    Secao 2 aula 15

--- aulaspring/src/main/java/com/gabrielgrs/aulaspring/AulaspringApplication.java
+++ aulaspring/src/main/java/com/gabrielgrs/aulaspring/AulaspringApplication.java
@@ -1,12 +1,32 @@
 package com.gabrielgrs.aulaspring;
 
+import com.gabrielgrs.aulaspring.domain.Categoria;
+import com.gabrielgrs.aulaspring.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class AulaspringApplication {
+public class AulaspringApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AulaspringApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+
+
 	}
 }
