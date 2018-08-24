6412b164b8d55bde9921ff921599b7f78a3ba11b
Gabriel Guilherme Rodrigues Silva <gabriel_guilherme2006@hotmail.com>
2018-08-23 21:05:26 -0300

    Secao 2 aula 17

--- aulaspring/src/main/java/com/gabrielgrs/aulaspring/services/CategoriaService.java
+++ aulaspring/src/main/java/com/gabrielgrs/aulaspring/services/CategoriaService.java
@@ -2,6 +2,7 @@ package com.gabrielgrs.aulaspring.services;
 
 import com.gabrielgrs.aulaspring.domain.Categoria;
 import com.gabrielgrs.aulaspring.repositories.CategoriaRepository;
+import com.gabrielgrs.aulaspring.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,10 +14,10 @@ public class CategoriaService {
     @Autowired
     private CategoriaRepository categoriaRepository;
 
-    public Categoria buscar(Integer id){
+    public Categoria buscar(Integer id) {
         Optional<Categoria> categoria = categoriaRepository.findById(id);
 
-
-        return categoria.orElse(null);
+        return categoria.orElseThrow(() -> new ObjectNotFoundException("Objeto nao encontrado! Id: " + id
+                + ", Tipo: " + Categoria.class.getName()));
     }
 }
---
+++ aulaspring/src/main/java/com/gabrielgrs/aulaspring/services/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,12 @@
+package com.gabrielgrs.aulaspring.services.exceptions;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+    public ObjectNotFoundException(String message) {
+        super(message);
+    }
+
+    public ObjectNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
