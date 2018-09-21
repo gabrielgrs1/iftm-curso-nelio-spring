ebe02189cdce210776eb416b03f7c76b4bce6aef
Gabriel Guilherme Rodrigues Silva <gabriel_guilherme2006@hotmail.com>
2018-09-20 21:13:56 -0300

    Secao 3 aula 35

--- aulaspring/src/main/java/com/gabrielgrs/aulaspring/resources/CategoriaResource.java
+++ aulaspring/src/main/java/com/gabrielgrs/aulaspring/resources/CategoriaResource.java
@@ -5,6 +5,7 @@ import com.gabrielgrs.aulaspring.domain.Categoria;
 import com.gabrielgrs.aulaspring.dto.CategoriaDTO;
 import com.gabrielgrs.aulaspring.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -62,4 +63,18 @@ public class CategoriaResource {
         List<CategoriaDTO> categoriaDTOList = categoriaList.stream().map(CategoriaDTO::new).collect(Collectors.toList());
         return ResponseEntity.ok().body(categoriaDTOList);
     }
+
+    @RequestMapping(value = "/page", method = RequestMethod.GET)
+    public ResponseEntity<Page<CategoriaDTO>> findPage(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+            @RequestParam(value = "orderBy", defaultValue = "nome") String orderBy,
+            @RequestParam(value = "direction", defaultValue = "ASC") String direction) {
+
+        Page<Categoria> categoriaPage = categoriaService.findPage(page, linesPerPage, orderBy, direction);
+        Page<CategoriaDTO> categoriaDTOList = categoriaPage.map(CategoriaDTO::new);
+        return ResponseEntity.ok().body(categoriaDTOList);
+    }
+
+
 }
