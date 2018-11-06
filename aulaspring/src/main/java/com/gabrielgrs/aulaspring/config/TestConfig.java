a20ced509e5469770879582ac13c549190e0dadf
Gabriel Guilherme Rodrigues Silva <gabriel_guilherme2006@hotmail.com>
2018-11-05 22:19:31 -0200

    Secao 3 aula 53

---
+++ aulaspring/src/main/java/com/gabrielgrs/aulaspring/config/TestConfig.java
@@ -0,0 +1,25 @@
+package com.gabrielgrs.aulaspring.config;
+
+import com.gabrielgrs.aulaspring.services.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.text.ParseException;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    @Bean
+    public boolean instantiatDatabase() throws ParseException {
+        dbService.instantiateTestDatabase();
+
+
+        return true;
+    }
+}
---
+++ aulaspring/src/main/java/com/gabrielgrs/aulaspring/services/DBService.java
@@ -0,0 +1,142 @@
+package com.gabrielgrs.aulaspring.services;
+
+import com.gabrielgrs.aulaspring.domain.*;
+import com.gabrielgrs.aulaspring.domain.enuns.EstadoPagamento;
+import com.gabrielgrs.aulaspring.domain.enuns.TipoCliente;
+import com.gabrielgrs.aulaspring.repositories.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Collections;
+
+@Service
+public class DBService {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    @Autowired
+    private CidadeRepository cidadeRepository;
+
+    @Autowired
+    private EstadoRepository estadoRepository;
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @Autowired
+    private EnderecoRepository enderecoRepository;
+
+    @Autowired
+    private PedidoRepository pedidoRepository;
+
+    @Autowired
+    private PagamentoRepository pagamentoRepository;
+
+    @Autowired
+    private ItemPedidoRepository itemPedidoRepository;
+
+    public void instantiateTestDatabase() throws ParseException {
+        Categoria cat1 = new Categoria("Informática");
+        Categoria cat2 = new Categoria("Escritório");
+        Categoria cat3 = new Categoria("Cama mesa e banho");
+        Categoria cat4 = new Categoria("Eletronicos");
+        Categoria cat5 = new Categoria("Jardinagem");
+        Categoria cat6 = new Categoria("Decoraçao");
+        Categoria cat7 = new Categoria("Perfumaria");
+
+        Produto p1 = new Produto("Computador", 2000.0);
+        Produto p2 = new Produto("Impressora", 800.0);
+        Produto p3 = new Produto("Mouse", 80.0);
+        Produto p4 = new Produto("Mesa de escritório", 80.0);
+        Produto p5 = new Produto("Toalha", 80.0);
+        Produto p6 = new Produto("Colcha", 80.0);
+        Produto p7 = new Produto("TV true color", 80.0);
+        Produto p8 = new Produto("Roçadeira", 80.0);
+        Produto p9 = new Produto("Abajour", 80.0);
+        Produto p10 = new Produto("Pendente", 80.0);
+        Produto p11 = new Produto("Shampoo", 80.0);
+
+        cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+        cat2.getProdutos().addAll(Arrays.asList(p2, p4));
+        cat3.getProdutos().addAll(Arrays.asList(p5, p6));
+        cat4.getProdutos().addAll(Arrays.asList(p1, p2, p3, p7));
+        cat5.getProdutos().addAll(Collections.singletonList(p8));
+        cat6.getProdutos().addAll(Arrays.asList(p9, p10));
+        cat7.getProdutos().addAll(Collections.singletonList(p11));
+
+        p1.getCategorias().addAll(Arrays.asList(cat1, cat4));
+        p2.getCategorias().addAll(Arrays.asList(cat1, cat2, cat4));
+        p3.getCategorias().addAll(Arrays.asList(cat1, cat4));
+        p4.getCategorias().addAll(Collections.singletonList(cat2));
+        p5.getCategorias().addAll(Collections.singletonList(cat3));
+        p6.getCategorias().addAll(Collections.singletonList(cat3));
+        p7.getCategorias().addAll(Collections.singletonList(cat4));
+        p8.getCategorias().addAll(Collections.singletonList(cat5));
+        p9.getCategorias().addAll(Collections.singletonList(cat6));
+        p10.getCategorias().addAll(Collections.singletonList(cat6));
+        p11.getCategorias().addAll(Collections.singletonList(cat7));
+
+        categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7));
+        produtoRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11));
+
+        Estado est1 = new Estado("Minas Gerais");
+        Estado est2 = new Estado("Sao Paulo");
+
+        Cidade c1 = new Cidade("Uberlandia", est1);
+        Cidade c2 = new Cidade("Sao Paulo", est2);
+        Cidade c3 = new Cidade("Campinas", est2);
+
+        est1.getCidades().addAll(Collections.singletonList(c1));
+        est1.getCidades().addAll(Arrays.asList(c1, c2, c3));
+
+        estadoRepository.saveAll(Arrays.asList(est1, est2));
+        cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+
+        Cliente cli1 = new Cliente("Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+
+        cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+
+        Endereco e1 = new Endereco("Rua Flores", "300", "Apto 303", "Jardim", "3220834", cli1, c1);
+        Endereco e2 = new Endereco("Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+
+        cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+
+        clienteRepository.saveAll(Collections.singletonList(cli1));
+        enderecoRepository.saveAll(Arrays.asList(e1, e2));
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyy HH:mm");
+        Pedido ped1 = new Pedido(simpleDateFormat.parse("30/09/2017 10:32"), cli1, e1);
+        Pedido ped2 = new Pedido(simpleDateFormat.parse("10/10/2017 19:35"), cli1, e2);
+
+        Pagamento pagto1 = new PagamentoComCartao(EstadoPagamento.QUITADO, ped1, 6);
+        ped1.setPagamento(pagto1);
+
+        Pagamento pagto2 = new PagamentoComBoleto(EstadoPagamento.PENDENTE, ped2, simpleDateFormat.parse("20/10/2017 00:00"), null);
+        ped2.setPagamento(pagto2);
+
+        cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+
+        pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+        pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+
+        ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+        ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+        ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+
+        ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+        ped2.getItens().addAll(Collections.singletonList(ip3));
+
+        p1.getItens().addAll(Collections.singletonList(ip1));
+        p2.getItens().addAll(Collections.singletonList(ip3));
+        p3.getItens().addAll(Collections.singletonList(ip2));
+
+        itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
+    }
+}
